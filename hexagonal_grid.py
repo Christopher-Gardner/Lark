4430198753a768e14f8a3bf2c6d4e3837b2a70fe
Chris <12497588+Christopher-Gardner@users.noreply.github.com>
2020-09-12 04:50:43 -0500

    Update hexagonal_grid.py

--- hexagonal_grid.py
+++ hexagonal_grid.py
@@ -1,5 +1,5 @@
-# drawing a hexagonal grid
-# a Python implementation of: Indera's Pearls, box 3
+# Drawing a hexagonal grid.
+# A Python implementation of "box 3" from Indera's Pearls.
 
 import random
 import math
@@ -7,24 +7,33 @@ from PIL import Image
 from PIL import ImageDraw
 
 
-def hexagon(center, radius):
-    v0 = (center[0], center[1] + radius)
-    v1 = (center[0] - radius * math.sqrt(3) / 2, center[1] + radius / 2)
-    v2 = (center[0] - radius * math.sqrt(3) / 2, center[1] - radius / 2)
-    v3 = (center[0], center[1] - radius)
-    v4 = (center[0] + radius * math.sqrt(3) / 2, center[1] - radius / 2)
-    v5 = (center[0] + radius * math.sqrt(3) / 2, center[1] + radius / 2)
-    return (v0, v1, v2, v3, v4, v5)
+def getHexagon(center, radius):
+    v0 = center[0], center[1] + radius
+    v1 = center[0] - radius * math.sqrt(3) / 2, center[1] + radius / 2
+    v2 = center[0] - radius * math.sqrt(3) / 2, center[1] - radius / 2
+    v3 = center[0], center[1] - radius
+    v4 = center[0] + radius * math.sqrt(3) / 2, center[1] - radius / 2
+    v5 = center[0] + radius * math.sqrt(3) / 2, center[1] + radius / 2
+    return v0, v1, v2, v3, v4, v5
 
 
+# translation along the x axis
 def genT(verts, radius, power):
     return tuple((x + radius * math.sqrt(3) * power, y) for (x, y) in verts)
 
 
+# translation along the line forming a pi / 2 angle with the x axis
 def genS(verts, radius, power):
     return tuple(
         (x + radius * math.sqrt(3) * power / 2, y + radius * 1.5 * power)
         for (x, y) in verts)
+
+
+def getRandomColor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
 
 def drawHexagonalGrid(radius, image):
@@ -32,19 +41,17 @@ def drawHexagonalGrid(radius, image):
     center = tuple(math.floor(x / 2) for x in image.size)
     # shrink the hexagons so the background color appears as an outline
     outline = math.ceil(radius / 10)
-    hexagonVerts = hexagon(center, radius - outline)
+    polygon = getHexagon(center, radius - outline)
     for i in range(-4, 5):
         for j in range(-2, 3):
             # compensate for the horizontal translation of the S generator
             iOffset = math.floor(j / 2)
-            translatedVerts = genT(hexagonVerts, radius, i - iOffset)
-            translatedVerts = genS(translatedVerts, radius, j)
-            color = (random.randint(0, 255), random.randint(0, 255),
-                     random.randint(0, 255))
-            draw.polygon(translatedVerts, fill=color)
+            translated = genT(polygon, radius, i - iOffset)
+            translated = genS(translated, radius, j)
+            color = getRandomColor()
+            draw.polygon(translated, fill=color)
 
 
-# higher quality results in crisper images
 quality = 300
 size = (16 * quality, 9 * quality)
 radius = math.ceil(quality / 1.5)
